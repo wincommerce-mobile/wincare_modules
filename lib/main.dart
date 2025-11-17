df9353e81178f1fa344727c6f435f4f85d040940
uytb <uytb@crownx.masangroup.com>
2025-11-17 10:54:18 +0700

    chore: update uat

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'app/global.dart';
 /// Environment
 String environment = const String.fromEnvironment(
   'ENV',
-  defaultValue: Environment.dev,
+  defaultValue: Environment.uat,
 );
 
 void main() {
